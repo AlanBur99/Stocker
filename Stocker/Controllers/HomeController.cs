9426b10c7f4866d361c14612c4a4d038a8dc973f
ALAN BURSZTYN <44553129@comunidadort.edu.ar>
2019-11-05 12:08:40 -0300

    cambio

--- Stocker/Controllers/HomeController.cs
+++ Stocker/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Stocker.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description.";
 
             return View();
         }
